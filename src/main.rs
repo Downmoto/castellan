35e9c2d34085aa0790958252c8fb0e4cbcd01e21
Arad Fadaei <aradfadaie@gmail.com>
2026-02-27 14:37:53 -0500

    Add dotenv support and main function with client interaction

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use castellan::logging::prelude::*;
 use castellan::settings::prelude::*;
 
+use rig::{client::{CompletionClient, ProviderClient}, providers::gemini::Client};
+use rig::completion::Prompt;
 use tracing::{Level, event, span};
+use dotenv::dotenv;
+
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
     let settings = settings();
     let _subscriber = logging_init(settings.app_log().level, settings.app_log().timestamp_mode);
 
@@ -14,4 +20,21 @@ async fn main() {
     if used_default_settings() {
         event!(Level::WARN, "Failed to parse configuration; using defaults")
     }
+
+    let client = Client::from_env();
+
+    let agent = client.agent("gemini-3-flash-preview")
+        .preamble("You are a helpful assistant.")
+        .name("Bob")
+        .build();
+
+    let prompt = "What is the Rust programming language?";
+    println!("{prompt}");
+
+    let response_text = agent.prompt(prompt).await?; 
+
+    println!("Response: {response_text}");
+
+    Ok(())
+
 }
