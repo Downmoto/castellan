2be932c2399734be2c28d8ae1ad12fc808781746
Arad Fadaei <aradfadaie@gmail.com>
2026-04-29 23:10:29 -0400

    re impl base modules

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,28 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+pub mod modules;
+
+use crate::modules::settings::{settings, used_default_settings};
+use crate::modules::tracing::logging_init;
+
+use dotenv::dotenv;
+
+use tracing::{span, event, Level};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    dotenv().ok();
+
+    let settings = settings();
+    let _subscriber = logging_init(settings.tracing.level, settings.tracing.timestamp_mode);
+
+    let _guard = span!(Level::INFO, "castellan_global").entered();
+    event!(Level::INFO, "App start");
+
+    if used_default_settings() {
+        event!(Level::WARN, "Failed to parse configuration; using defaults");
+    }
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
